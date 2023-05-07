dc17cfe0b726fa18efd3b2753a7bd8ff75244a5b
Anvar571 <abdurashidovanvar52@gmail.com>
2023-05-07 22:52:05 +0500

    initial

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Design-patterns
 design-patterns larni o'rganish va ular bilan pet projectlar qilish uchun bu repo
+
+
+design pattern
+    creational pattern
+        factory method
+        abstract factory
+        builder
+        prototype
+        singleton
+    structural pattern
+        adabter
+        bridge
+        composite
+        decorator
+        facade
+        proxy
+    behavioral pattern
+        command
+        iterator
+        memento
+        state
+        visitor
+        strategy
+        template method
+        observer
---
+++ js/creational/builder/builder.js
@@ -0,0 +1,23 @@
+// uy class 
+
+// muammolar yoritilishi kerak 
+// va o'sha muammolarga yechimlar yoritilishi kerak
+
+// baseni bor uy
+// bogi bor
+// moziy bor
+// garaj bor
+// yana qo'shimchalari bor masalan(isitish tizimi, dush, )
+// buni churish uchun juda ham ko'p class ishlatish mumkin lekin classlarning soni ko'bayib ketadi
+// buni oldini olish methodlardan foydalanish buni ham yomon tomoni bazi methodlar kerak emas bazida
+
+// buning uchun yechim uy quradigan class yozish unda bazi methodlar privete asosiylari public
+
+
+// Turli quruvchilar bir xil vazifani turli yo'llar bilan bajaradilar.
+
+// Direktor ishlaydigan mahsulotni olish uchun qaysi qurilish bosqichlarini bajarish kerakligini biladi.
+
+// Bundan tashqari, direktor klassi mahsulot qurilishi tafsilotlarini mijoz kodidan butunlay yashiradi. 
+//Mijoz faqat quruvchini direktor bilan bog'lashi, 
+//direktor bilan qurilishni boshlashi va quruvchidan natija olishi kerak.
